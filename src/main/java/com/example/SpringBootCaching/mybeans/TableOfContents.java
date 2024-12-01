82ab746257db1f11a650d80f3394a7dd03f998a4
Michael D Clayton <michael.d.clayton@hotmail.com>
2024-11-30 19:19:33 -0500

    updated logger, point to "this" class.

--- src/main/java/com/example/SpringBootCaching/mybeans/TableOfContents.java
+++ src/main/java/com/example/SpringBootCaching/mybeans/TableOfContents.java
@@ -1,11 +1,10 @@
 package com.example.SpringBootCaching.mybeans;
 
-import com.example.SpringBootCaching.AppRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TableOfContents {
-    private static final Logger logger = LoggerFactory.getLogger(AppRunner.class);
+    private static final Logger logger = LoggerFactory.getLogger(TableOfContents.class);
 
 
     public TableOfContents() {
